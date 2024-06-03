05720ec1ff382fbe6a8405afd8b7457cb515a5fe
lokesh patidar <lpatidar00@gmail.com>
2024-06-03 19:14:23 +0530

    E-Com FrontEnd Part

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="description-box">
+      <div className="description-box-navigator">
+        <div className="description-box-nav-box">Description</div>
+        <div className="description-box-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="description-box-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          inventore facere deserunt magni! Repellat harum alias deserunt, et
+          voluptatum ut vero consequatur aliquid laboriosam corrupti enim
+          excepturi ipsa qui Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Cum modi perferendis, sit deleniti nulla sint unde rem
+          repudiandae nisi beatae Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Possimus perspiciatis rem, eligendi deleniti
+          repudiandae officia neque placeat nam ipsam debitis soluta fugiat
+          veritatis est dolore totam facilis. Laudantium pariatur quasi a? Illum
+          repellat dicta minus hic quis! Itaque voluptate, reiciendis nam
+          officia quis reprehenderit quisquam nisi? Perferendis repellendus nemo
+          minus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+          ducimus illo architecto nulla esse doloribus hic quaerat maiores enim
+          delectus nesciunt aliquam quibusdam perferendis commodi facere aut
+          sunt, possimus quisquam numquam illum voluptatibus corporis.
+          Veritatis, accusamus consequuntur facilis excepturi saepe itaque sit
+          nesciunt, numquam nam officia optio magnam. Molestias, pariatur.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
